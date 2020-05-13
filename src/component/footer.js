e50bbbf97abec59f170501446df886b332758df3
Felipe Nathan Campigoto <felipe.campigoto@varejonline.com.br>
2020-05-13 19:11:29 -0300

    adding link to github

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { Link } from '@reach/router'
-import { GrTwitter, GrLinkedin, GrFacebook } from 'react-icons/gr'
+import { GrTwitter, GrLinkedin, GrFacebook, GrGithub } from 'react-icons/gr'
 
 export default function Footer() {
 
@@ -18,18 +18,25 @@ export default function Footer() {
                         <Col className="d-flex justify-content-end">
                             <a href="https://twitter.com/LipeCampigoto"
                                 target="_blank"
-                                className="mr-3 text-decoration-none text-light" 
+                                className="mr-3 text-decoration-none text-light"
                                 rel="noopener noreferrer"><GrTwitter /></a>
 
                             <a href="https://www.linkedin.com/in/felipe-nathan-campigoto-18a3355a/"
                                 target="_blank"
-                                className="mr-3 text-decoration-none text-light" 
+                                className="mr-3 text-decoration-none text-light"
                                 rel="noopener noreferrer"><GrLinkedin /></a>
 
                             <a href="https://www.facebook.com/ncampigoto"
                                 target="_blank"
-                                className="mr-3 text-decoration-none text-light" 
+                                className="mr-3 text-decoration-none text-light"
                                 rel="noopener noreferrer"><GrFacebook /></a>
+
+                            <a href="https://github.com/felipenathan/"
+                                target="_blank"
+                                className="mr-3 text-decoration-none text-light"
+                                rel="noopener noreferrer"><GrGithub /></a>
+
+
                         </Col>
                     </Row>
                 </Container>
